7276cbb8a49cdaafae1c65708ab56671d7e4fc15
sem2205 <smroz040@uottawa.ca>
2021-09-27 14:38:35 -0400

    Add Cube Game Unity Project

---
+++ Cube Game/Assets/IndividualControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IndividualControl : MonoBehaviour
+{
+
+    private Camera myCam;    
+    bool MouseActive = false;
+    public float RotationSpeed = 1.0f;
+    public float lerpDuration = 1000f;
+    public int MouseCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Cube will spin when mouse is pressed down
+        if (Input.GetMouseButton(0))
+        {
+            if (MouseActive == true)
+            {
+                   StartCoroutine(RotateCubes());
+
+                }
+        }
+       
+    }
+    //Called when user has pressed the mouse button down while over an object
+    void OnMouseDown()
+    {
+        MouseActive = true;
+        MouseCount = MouseCount + 1;
+    }
+
+    void OnMouseUp()
+    {
+        MouseActive = false;
+    }
+    IEnumerator RotateCubes()
+    {
+               
+        float timeElapsed = 0;
+                
+        while (timeElapsed < lerpDuration)
+        {
+            if (MouseCount % 2 == 1)
+            {
+                this.transform.Rotate(Vector3.up * (RotationSpeed+10f) * Time.deltaTime);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+            if (MouseCount % 2 == 0)
+            {
+                this.transform.Rotate(0,0,0);
+                timeElapsed = lerpDuration+1;
+            }
+
+        }
+       
+    }
+}
+
+
---
+++ Cube Game/Assets/Scripts/Controls.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+
+{
+    public GameObject cubePlayer;
+    public List<GameObject> cubeClones;
+       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeClones = new List<GameObject>();
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+         Vector3 position = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), Random.Range(-10, 10f));                
+         GameObject newCube = Instantiate(cubePlayer, position, Quaternion.identity) as GameObject;
+         cubeClones.Add(newCube);
+            Debug.Log("The length of the list of cubes is: " + cubeClones.Count);           
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            foreach (GameObject newCube in cubeClones)
+            {
+                newCube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            foreach (GameObject newCube in cubeClones)
+            {
+                Vector3 position = newCube.transform.position;
+                position.x--;
+                newCube.transform.position = position;
+            }
+                
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            foreach (GameObject newCube in cubeClones)
+            {
+                Vector3 position = newCube.transform.position;
+                position.x++;
+                newCube.transform.position = position;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            foreach (GameObject newCube in cubeClones)
+            {
+                Vector3 position = newCube.transform.position;
+                position.y++;
+                newCube.transform.position = position;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            foreach (GameObject newCube in cubeClones)
+            {
+                Vector3 position = newCube.transform.position;
+                position.y--;
+                newCube.transform.position = position;
+            }
+
+        }
+       
+    }
+
+   
+}
+
